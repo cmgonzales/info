098f42bf694875eec1cd23635d46437d926f02bc
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-08-23 20:34:03 -0700

    testing background

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
+
 var stars = 20000;
+
+var renderer;
 var container = document.createElement('div');
 document.body.appendChild( container );
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,1, 4000)
+var camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight,1, 4000)
 camera.position.z = 1000; 
 
-var renderer = new THREE.WebGLRenderer();
+var scene = new THREE.Scene();
+
+scene.add(camera)
+
+ renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild( renderer.domElement );
 
@@ -14,8 +21,6 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
-var mouseX = 0;
-var mouseY = 0;
 
 
 var scene = new THREE.Scene();
@@ -36,11 +41,16 @@ var particles = new THREE.ParticleSystem( geometry, material );
       
 scene.add( particles ); 
 
-camera.position.z = 1000;
+
+
+camera.position.z = 13000;
 
 
 
 renderer.render( scene, camera );
+
+
+
 
 
 function onMouseMove( event ) {
@@ -50,8 +60,15 @@ function onMouseMove( event ) {
 }
 
 
+
+update(){
+updateParticles()
+}
+
+
+
+
 document.addEventListener( 'mousemove', onMouseMove, false );
 setInterval(update,1000/30); 
-
 
 
